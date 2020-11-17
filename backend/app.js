6ee1eb7a403de7ea3dc324b29320656b91913876
cecile gaubert <primaelswann@gmail.com>
2020-11-17 16:53:18 +0100

    dossier backend

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+/*framework*/
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+const helmet = require('helmet');
+
+const config = require('./config')
+
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+const app = express();
+mongoose.connect(config.mongoURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((_req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ backend/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Cissou0511:CG0511ps@cluster0.qvfwm.mongodb.net/pecocko?retryWrites=true&w=majority'
+}
